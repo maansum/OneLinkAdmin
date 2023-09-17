66c240f3a97867e6076c85448a84320281ef235e
unknown <sumandarji2479@gmail.com>
2023-09-17 14:42:48 +0545

    dommy data

--- lib/utils/dommy_data.dart
+++ lib/utils/dommy_data.dart
@@ -1,26 +1,43 @@
 import 'package:onelinkadmin/models/review_model.dart';
 
-final List<Review> dummyReviews = [
+List<Review> dummyReviews = [
   Review(
-    username: 'User1',
-    mediaImage: 'assets/images/facebook.png',
-    date: DateTime(2023, 9, 14),
+    username: 'Ibrahim Awadallah',
+    mediaImage: 'assets/images/facebook.png', // Add media image URL
+    date: DateTime.now().subtract(Duration(days: 150)), // 5 months ago
     rating: 5,
     comment:
-        'This app is amazing!This app is amazing!This app is amazing!This app is amazing!This app is amazing!',
+        'Good food and reasonable prices. My only problem is in the pickup process...',
   ),
   Review(
-    username: 'User2',
+    username: 'Chris VanDercook',
     mediaImage: 'assets/images/facebook.png',
-    date: DateTime(2023, 9, 15),
-    rating: 5.0,
-    comment: 'The best app I have ever used!',
+    date: DateTime.now().subtract(Duration(days: 21)), // 3 weeks ago
+    rating: 4,
+    comment:
+        'I noticed recently that their dining room is back open for the first time since the pandemic...',
   ),
   Review(
-    username: 'User3',
+    username: 'Sudan Coleman',
     mediaImage: 'assets/images/facebook.png',
-    date: DateTime(2023, 9, 16),
-    rating: 3.8,
-    comment: "It's pretty good, but could use some improvements.",
+    date: DateTime.now().subtract(Duration(days: 30)), // a month ago
+    rating: 1,
+    comment: 'Used to be a great place to eat, not anymore...',
+  ),
+  Review(
+    username: 'Summer',
+    mediaImage: 'assets/images/facebook.png',
+    date: DateTime.now().subtract(Duration(days: 270)), // 9 months ago
+    rating: 5,
+    comment:
+        'We already smelled the curry when across the street haha. Food is very delicious...',
+  ),
+  Review(
+    username: 'Aravind Radhakrishna',
+    mediaImage: 'assets/images/facebook.png',
+    date: DateTime.now().subtract(Duration(days: 1095)), // 3 years ago
+    rating: 5,
+    comment:
+        'The quality of food and taste is great also pretty good hygiene. The service is also nice. Polite Staff is always willing to help. What makes them better from other Indian restaurants is that they are consistent in their quality and service.. So far one of the best Indian restaurants in Charlotte, I love to visit them again. Keep up the good work.',
   ),
 ];
